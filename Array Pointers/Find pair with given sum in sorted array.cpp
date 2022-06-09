72b61ea81b3b29fc07e15efd5acdc5fc5cf5b884
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2022-06-09 23:52:54 +0530

    Create Find pair with given sum in sorted array.cpp

---
+++ Array Pointers/Find pair with given sum in sorted array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool twoSumInSortedArray(int n, vector<int> arr, int target){
+    int i =0,j = n-1;
+    while(i<j){
+        int sum = arr[i] + arr[j];
+        if(sum == target)
+            return true;
+        if(sum>target)
+            j--;
+        else
+            i++;
+    }
+    return false;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int > arr(n);
+    for(int i=0; i<n ;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin >> target;
+    bool ans = twoSumInSortedArray(n, arr, target);
+    cout << (ans ? "Present" : "Not Present") << "\n";
+}

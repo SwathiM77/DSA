c3c4a9e9ea484aa39d74df101f2f9fe64cb9f21e
SwathiM77 <47594298+SwathiM77@users.noreply.github.com>
2022-06-24 00:43:38 +0530

    Create Find LCA in Binary Tree.cpp

---
+++ Binary Search/Find LCA in Binary Tree.cpp
@@ -0,0 +1,48 @@
+// Definition for TreeNode
+/*
+class TreeNode {
+public:
+    long val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode* next;
+    TreeNode (long x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+        next = NULL;
+    }
+*/
+
+bool isPresent(TreeNode* root,int val){
+    if(!root)
+        return false;
+    if(root->val == val)
+        return true;
+    return (isPresent(root->left,val) || isPresent(root->right,val));
+}
+
+class CommonAncestor {
+public:
+    TreeNode* commonAncestor(TreeNode* root, TreeNode * p , TreeNode* q) {
+        if(!root || !p || !q)
+            return NULL;
+
+        if(root->val == p->val && (isPresent(root->left,q->val) || isPresent(root->right,q->val)))
+            return root;
+        if(root->val == q->val && (isPresent(root->left,p->val) || isPresent(root->right,p->val)))
+            return root;
+        
+        if(isPresent(root->left,p->val) && isPresent(root->right,q->val))
+            return root;
+        if(isPresent(root->left,q->val) && isPresent(root->right,p->val))
+            return root;
+
+        TreeNode* left = commonAncestor(root->left,p,q);
+        TreeNode* right = commonAncestor(root->right,p,q);
+
+        if(left)
+            return left;
+        return right;
+    }
+};
